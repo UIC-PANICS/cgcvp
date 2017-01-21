1ebe7c65f5db41f29fed6569c0131f8f4bfef0bb
ecwu <bitlabwzh@gmail.com>
2017-01-21 22:04:30 +0800

    mod header

--- header.php
+++ header.php
@@ -1,33 +1,3 @@
-<?php
-
-if (@$_COOKIE['login'] != $url)
-{
-echo <<<nlogin
-
-    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#">UIC CGCVP</a>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="drj">Status</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-nlogin;
-}
-else 
-
-	echo  <<<logined
-
     <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -45,13 +15,12 @@ else
         </ul>
       </div>
         <form class="form-inline my-2 my-lg-0" style="width: auto;">
-        <a class="btn btn-outline-warning btn-block" href="/logout.php">Login as: <?php echo $_COOKIE["login"];?></a>
-           
-        </form>
-    </nav>
-	
-logined;
-	
-}
+<!-- Php part -->
+        <?php
+        if (@$_COOKIE['login'] == "admin"){
+          echo "<a class='btn btn-outline-warning btn-block' href='/logout.php'>"."Login as: ". @$_COOKIE['login']."</a>";
+        }
+        ?>
 
-?>
+        </form>
+    </nav> 
