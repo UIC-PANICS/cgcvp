9735493f266b9cc87c7f62dc8b2df7b633cf1839
ecwu <bitlabwzh@gmail.com>
2017-01-21 20:40:22 +0800

    add a unexecutable file for review

--- header.php
+++ header.php
@@ -1,30 +1,57 @@
 <?php
-echo <<<END
 
-
-
-END;
-
-
-if (isset($_SESSION['user']))
+if (@$_COOKIE['login'] != $url)
 {
-	$user     = $_SESSION['user'];
-	$loggedin = TRUE;
+/*echo <<<nlogin*/
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">UIC CGCVP</a>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="drj">Status</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+/*nlogin;*/
 }
-else $loggedin = FALSE;
+else 
 
-if ($loggedin)
-{
-	echo  <<<END
+/*	echo  <<<logined*/
 
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">UIC CGCVP</a>
 
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="">Status<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+        <form class="form-inline my-2 my-lg-0" style="width: auto;">
+        <a class="btn btn-outline-warning btn-block" href="/logout.php">Login as: <?php echo $_COOKIE["login"];?></a>
+           
+        </form>
+    </nav>
 	
-END;
+/*logined;*/
 	
-}
-else
-{
-	redirect('login.php');
 }
 
 ?>
